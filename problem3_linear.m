50877198db9d49a069d7273d189063f7517cc1fa
Marc Hauptmann <marc.hauptmann@stud.uni-hannover.de>
2013-09-25 15:28:36 +0200

    Problem 3 angepasst

--- problem3_linear.m
+++ problem3_linear.m
@@ -1,4 +1,4 @@
-function problem3_multigrid
+function problem3_linear
 % löst -u'' = 1 mit linearen Basisfunktionen
 
 a = 0;
@@ -6,9 +6,9 @@ b = 1;
 ul = 0;
 ur = 0;
 
-n = 128;
-elements = linspace(a, b, n+1);
-h = 1/n;
+n = 129;
+h = 1/(n-1);
+elements = linspace(a, b, n);
 
 basis = {@(x) 1-x, @(x) x};
 
@@ -16,29 +16,35 @@ basis = {@(x) 1-x, @(x) x};
 k = [ 1 -1
      -1  1];
 
-tic 
-
 % node ordering
-B = zeros(n, 2);
+B = zeros(n-1, 2);
 
-parfor i=1:n
+parfor i=1:n-1
     B(i, :) = [i, i+1];
 end
 
 % Assemblierung
+tic
+
 A = createMatrix(k, B, 1/h);
-fb = createVector(basis, B, linspace(a, b, n+1), @(x) 1);
+fb = createVector(basis, B, elements, @(x) 1);
 
 % Randbedingungen beachten
-[A, fb] = dirichletBoundary(A, fb, [1, n+1], [ul, ur]);
+[A, fb] = dirichletBoundary(A, fb, [1, n], [ul, ur]);
 
 % lösen
-u = multigridSolve(A, fb, ceil(log2(n)));
+[Dinv, L, R] = jacobiDecompose(A);
+
+u = zeros(length(fb), 1);
+
+while norm(A*u - fb) > 1e-4
+    u = Dinv*(fb-(L+R)*u);
+end
 
 toc
 
 % exakte Lösung
-x = linspace(a, b, 1000);
+x = linspace(a, b, 2000);
 y = -1/2*(x-1).*x;
 
 % Basisfunktionen
@@ -48,6 +54,4 @@ Phi = createBasis(basis, elements, x, B);
 plot(x, Phi*u, x, y);
 legend('Näherung', 'exakt', 'location', 'Best');
 
-fprintf('Fehler: %d\n', norm(Phi*u-y'));
-
-end
+fprintf('Fehler: %s\n', norm(Phi*u - y'));
--- src/multigridSolve.m
+++ src/multigridSolve.m
@@ -5,29 +5,34 @@ function u = multigridSolve(A, b, level)
     if level == 1
         u = A\b;
     else
+        u = zeros(dim,1);
         [Dinv, L, R] = jacobiDecompose(A);
         
-        u = zeros(dim,1);
-        
-        for i=1:100
+        for i=1:3
             u = Dinv*(b-(L+R)*u);
         end
 
         rest = restriction(dim-2);
         [m,n] = size(rest);
-        
+
         r2 = zeros(m+2, n+2);
         r2(2:m+1,2:n+1) = rest;
         r2(1,1) = 2;
         r2(m+2,n+2) = 2;
-        r = 1 * r2;
+        r = 0.5 * r2;
+
+        if r(m+1,n+1) == 0
+            error('Element 0');
+        end
         
         p = r2';
         p(1:2,1) = [1; 0.5];
         p(n+1:n+2,m+2) = [0.5; 1];
+            
+        for i=1:2
+            u = u - p * multigridSolve(r*A*p, r*(A*u - b), level-1);
+        end
         
-        u = u - p * multigridSolve(r*A*p, r*(A*u - b), level-1);
-
         for i=1:3
             u = Dinv*(b-(L+R)*u);
         end
